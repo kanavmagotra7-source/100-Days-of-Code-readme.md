3ade5685b9413a5266f90c9c9587717694e38bad
Kanav Magotra <kanavmagotra7@gmail.com>
2025-09-23 12:42:05 +0530

    Question 22:Write a program to find profit or loss percentage given cost price and selling price.c

--- Question 22:Write a program to find profit or loss percentage given cost price and selling price.c
+++ Question 22:Write a program to find profit or loss percentage given cost price and selling price.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    float cp, sp, percent;
+
+    printf("Enter Cost Price: ");
+    scanf("%f", &cp);
+
+    printf("Enter Selling Price: ");
+    scanf("%f", &sp);
+
+    if(sp > cp) {
+        percent = ((sp - cp) / cp) * 100;
+        printf("Profit Percentage = %.2f%%\n", percent);
+    }
+    else if(sp < cp) {
+        percent = ((cp - sp) / cp) * 100;
+        printf("Loss Percentage = %.2f%%\n", percent);
+    }
+    else {
+        printf("No Profit, No Loss\n");
+    }
+
+    return 0;
+}
 
