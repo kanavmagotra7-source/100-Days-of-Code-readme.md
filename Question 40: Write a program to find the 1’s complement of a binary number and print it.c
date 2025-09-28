d3c024c8a5178c987bb86b8ac8f6b0d6105d9315
Kanav Magotra <kanavmagotra7@gmail.com>
2025-09-28 22:46:56 +0530

    Create Question 40: Write a program to find the 1’s complement of a binary number and print it.c

---
+++ Question 40: Write a program to find the 1’s complement of a binary number and print it.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char binary[32];
+    int i;
+
+    printf("Enter a binary number: ");
+    scanf("%s", binary);
+
+    printf("1's complement = ");
+    for(i = 0; binary[i] != '\0'; i++) {
+        if(binary[i] == '0')
+            printf("1");
+        else if(binary[i] == '1')
+            printf("0");
+        else {
+            printf("\nInvalid binary number\n");
+            return 0;
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
